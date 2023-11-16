c8d74adac3401252e12ab8f82ee6d705f045957c
Brett Meyer <32486568+bmeyer0127@users.noreply.github.com>
2023-11-16 13:52:07 -0500

    Delete CH08 directory
    Resetting files with current and up-to-date versions

--- CH08/CodeAlongs/Teams_And_Captains.sql
+++
@@ -1,9 +0,0 @@
-USE BowlingLeagueExample
-SELECT
-	Teams.TeamName,
-	Bowlers.BowlerFirstName + ' ' + Bowlers.BowlerLastName AS NameOfCaptain
-FROM 
-	Teams
-	JOIN
-	Bowlers
-	ON Teams.CaptainID = Bowlers.BowlerID
